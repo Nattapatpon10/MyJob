fd69d99fb4521b28ceae2df167e0d1db1916aa9d
Nattapatpon10 <nnatnaja2@gmail.com>
2022-08-10 11:49:23 +0700

    select 3 choice

--- Myjob.java
+++ Myjob.java
@@ -2,8 +2,8 @@ public class Myjob {
     public static void main(String[] args) {
         System.out.println("Software Engineer Frontend Developer (Digital Banking) \nSiam Commercial Bank Public Co., Ltd. (SCB)");
         System.out.println("Job Descriptions");
-        System.out.println(" - Analyze, design, develop, test, and deploy software. \n - Maintain high standards code quality and keep up with industry best practices. \n - Review code and provide feedback.\n - Work closely with product team to understand the business requirement and identify what need to be improved.");
+        System.out.println(" - Analyze, design, develop, test, and deploy software. \n - Review code and provide feedback.\n - Work closely with product team to understand the business requirement and identify what need to be improved.");
         System.out.println("Qualifications");
-        System.out.println(" - 1 year of experience with front-end web technologies, e.g., HTML, CSS, JavaScript, TypeScript. \n - 1 year of experience with web framework/library, e.g., React, Angular, Vue.js. \n - Familiarity with development tools, e.g., Git, Docker, and Jenkins. \n - Strong knowledge in programming fundamental, data structure, and algorithm. \n - Ability to work in rapid change environment and willingness to learn new things. \n - Experience with software development on cloud service is a plus. \n - Experience with mobile development is a plus");
+        System.out.println(" - 1 year of experience with front-end web technologies, e.g., HTML, CSS, JavaScript, TypeScript. \n - Experience with software development on cloud service is a plus. \n - Experience with mobile development is a plus");
     } 
 }
