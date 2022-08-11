938297c5b53a83fd03a385b1cc221ab71f9cba6a
Nattapatpon10 <nnatnaja2@gmail.com>
2022-08-11 13:42:17 +0700

    Translate

--- Myjob.java
+++ Myjob.java
@@ -1,9 +1,13 @@
 public class Myjob {
     public static void main(String[] args) {
         System.out.println("Software Engineer Frontend Developer (Digital Banking) \nSiam Commercial Bank Public Co., Ltd. (SCB)");
-        System.out.println("Job Descriptions");
-        System.out.println(" - Analyze, design, develop, test, and deploy software. \n - Review code and provide feedback.\n - Work closely with product team to understand the business requirement and identify what need to be improved.");
+        System.out.println("Job Descriptions");   
+        System.out.println(" - Analyze, design, develop, test, and deploy software.");
+        System.out.println(" - Review code and provide feedback.");
+        System.out.println(" - Work closely with product team to understand the business requirement and identify what need to be improved.");
         System.out.println("Qualifications");
-        System.out.println(" - 1 year of experience with front-end web technologies, e.g., HTML, CSS, JavaScript, TypeScript. \n - Experience with software development on cloud service is a plus. \n - Experience with mobile development is a plus");
+        System.out.println(" - 1 year of experience with front-end web technologies, e.g., HTML, CSS, JavaScript, TypeScript.");
+        System.out.println(" - Experience with software development on cloud service is a plus. ");
+        System.out.println(" - Experience with mobile development is a plus");
     } 
 }
